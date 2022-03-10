2d712ab7cbcabff02ddd435d3a37cd31b89326b1
Felipe Leal <lealfelipealves@gmail.com>
2022-03-09 23:12:13 -0300

    refactor: adding routes in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { useEffect } from "react";
-import { api } from "./services/api";
 import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
+import Routes from './routes';
  
 export function App() {
-  useEffect(() => {
-    api.get('neo/rest/v1/feed', { 
-      params: {
-        'start_date': '2022-01-01',
-        'end_date': '2022-01-08',
-        'api_key': process.env.REACT_APP_NASA_API_KEY
-      } 
-    })
-      .then(response => console.log(response.data))
-  }, []);
+  
 
   return (
     <div className="App">
-      <h1>Hola!</h1>
+      <Header />
+      <Routes />
       <GlobalStyle />
     </div>
   );
