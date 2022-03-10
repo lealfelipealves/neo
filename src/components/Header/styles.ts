d422c5397f209593ce3fa4528931e87b6aac40fa
Felipe Leal <lealfelipealves@gmail.com>
2022-03-09 23:08:05 -0300

    feat: header component creation and styling

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background: var(--blue);
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+
+  padding: 2rem 1rem 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 { 
+    color: #FFF;
+  }
+`;
