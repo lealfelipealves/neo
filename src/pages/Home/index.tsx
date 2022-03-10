8d145b194b33cfd20451cc2022bbda4d79e73604
Felipe Leal <lealfelipealves@gmail.com>
2022-03-09 23:09:51 -0300

    feat: home page creation and styling

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+import { NeoTable } from '../../components/NeoTable'
+
+export function Home() {
+  return (
+    <Container>
+      <NeoTable />
+    </Container>
+  );
+}
