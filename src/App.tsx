d99dd542d9b0163db322bfcba66cff46bae49598
Felipe Leal <lealfelipealves@gmail.com>
2022-03-09 21:43:25 -0300

    chore: made the call on the endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { api } from "./services/api";
 import { GlobalStyle } from "./styles/global";
  
 export function App() {
+  useEffect(() => {
+    api.get('neo/rest/v1/feed', { 
+      params: {
+        'start_date': '2022-01-01',
+        'end_date': '2022-01-08',
+        'api_key': process.env.REACT_APP_NASA_API_KEY
+      } 
+    })
+      .then(response => console.log(response.data))
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <h1>Hola!</h1>
       <GlobalStyle />
     </div>
   );
